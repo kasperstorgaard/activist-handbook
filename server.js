308e0ef097dbadd22bd28b0c4b490a17272528c4
kasper.storgaard <kasper.storgaard@gmail.com>
2017-08-28 17:49:34 +0200

    refactor(*): swap npm for bower (was too soon)

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 // Static folders
 app.use('/libs', express.static(__dirname + '/libs'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use('/src', express.static(__dirname + '/src'));
 
 // Routes
