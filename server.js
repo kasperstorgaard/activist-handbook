1ecfcb3ddd715db6bfd321f8bf97f9e492feb49d
kasper.storgaard <kasper.storgaard@gmail.com>
2017-08-29 09:22:54 +0200

    feat(flags): added flags, style slightly

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use('/libs', express.static(__dirname + '/libs'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use('/src', express.static(__dirname + '/src'));
+app.use('/static', express.static(__dirname + '/static'));
 
 // Routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
