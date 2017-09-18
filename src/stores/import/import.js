5f700ef94e32f425ba242c80e0e282f6996cae23
kasper.storgaard <kasper.storgaard@gmail.com>
2017-09-18 19:15:47 +0200

    refactor: removes import code duplication

--- src/stores/import/import.js
+++ src/stores/import/import.js
@@ -13,9 +13,9 @@ const done = createAction(DONE);
 const error = createAction(ERROR);
 
 async function createCountry(country) {
-  const regions = country.regions != null ? `["${country.regions.join('", "')}"]` : null;
-  const code = country.code != null ? `["${country.code.join('", "')}"]` : null;
-  const position = country.position != null ? `[${country.position.join(', ')}]` : null;
+  const regions = country.regions != null ? JSON.stringify(country.regions) : null;
+  const code = country.code != null ? JSON.stringify(country.code) : null;
+  const position = country.position != null ? JSON.stringify(country.position) : null;
 
   await queryGraphQL(
   `mutation {
