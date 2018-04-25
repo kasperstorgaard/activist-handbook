7c7fde2cca32c4cb01dc59dbaad357350eb33328
kstorgaard <kst@kayak.com>
2018-04-25 12:12:08 +0200

    stuff

--- dangerfile.ts
+++ dangerfile.ts
@@ -4,4 +4,4 @@ const modifiedFiles = danger.git.modified_files
     .map(file => `- ${file}`)
     .join('\n');
 
-message("Changed Files in this PR: \n" + modifiedFiles);
+message("Changed stuff in this PR: \n" + modifiedFiles);
