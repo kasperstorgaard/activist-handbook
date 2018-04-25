3fd12bb0dcc9eb091167c591406b0ba09c266824
kstorgaard <kst@kayak.com>
2018-04-25 12:14:52 +0200

    fail

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,7 +1,9 @@
-import {message, danger} from "danger";
+import {message, fail, danger} from "danger";
 
 const modifiedFiles = danger.git.modified_files
     .map(file => `- ${file}`)
     .join('\n');
 
 message("Changed stuff in this PR: \n" + modifiedFiles);
+
+fail("sandholt doesn't like this!");
